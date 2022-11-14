e1a20f49d6749a7737319c80a173e344acfe3b6c
DAVID HO <davidhorph@hotmail.com>
2022-11-14 09:26:07 -0600

    Inside Application.js: dispatched the action via dispatch = useDispatch( ); and then dispatch( increment( ) ); or dispatch( decrement( ) ); IMPORTANTgit status! Must call that action inside the dispatch using ( ) or it will NOT work.

--- src/reduxtoolkit/Application.js
+++ src/reduxtoolkit/Application.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import '../App.css'
 import '../index.css'
+import { decrement, increment, incrementByAmount } from './counter';
 
 const Application = () => {
     //https://www.youtube.com/watch?v=iBUJVy8phqw
-    //15:51
+    //16:03
 
     //useSelector is similar to mapStateToProps where we get the state (or a certain part of it).
     const { count } = useSelector(state => state.counter);
-
+    const dispatch = useDispatch();
+    //To call an action: dispatch(action());
     return (
         <div>
             <h1>Redux Tutorial (with Redux Toolkit)</h1>
             <h3>The Net Ninja.</h3>
             <div className='App'>
                 <h1>The count is: { JSON.stringify(count) }.</h1>
-                <button onClick={() => {}}>INCREMENT.</button>
-                <button onClick={() => {}}>DECREMENT.</button>
+                <button onClick={() => dispatch(increment())}>INCREMENT.</button>
+                <button onClick={() => dispatch(decrement())}>DECREMENT.</button>
             </div>
         </div>
     )
