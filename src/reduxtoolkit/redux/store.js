516df2b5382aa1fd09da67f22e5f838240702b4d
DAVID HO <davidhorph@hotmail.com>
2022-11-14 08:45:47 -0600

    Inisde store.js: added counterReducer to the reducer object in configureStore. counterReducer is actually the export default counterSlice.reducer which I renamed (for now) to counterReducer (you can rename like this for export default).

--- src/reduxtoolkit/redux/store.js
+++ src/reduxtoolkit/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../counter";
 
 //For now, we will only have 1 store designed to hold the count.
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        counterReducer //This was the export default counterSlice.reducer.
+    }
 })
 
 export default store;
